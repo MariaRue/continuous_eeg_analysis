56d3e38efdecce52ff311b14103e2c35b4e668a2
lhuntneuro <laurence.hunt@psych.ox.ac.uk>
2022-03-10 15:03:42 +0000

    minor changes to plot_convolution_GLMs

--- eeg_analysis_clean/figures_for_paper.m
+++ eeg_analysis_clean/figures_for_paper.m
@@ -1,10 +1,12 @@
 % load plot variables 
 addpath(genpath(pwd));
-options = continuous_RDK_set_options('iMac');
-plotStructure;
+options = continuous_RDK_set_options('LTHiMac');
+plotStructure; %returns plotVariables to workspace
 
-%% 
+%% figure 1: plots prediction error effect sorted by long vs. short, rare vs. frequent
 make_figure1(plotVariables,options); 
 
-%% 
+%% figure 2: plots different response-locked ERPs in run-up to making a final choice
+
 make_figure2(plotVariables,options); 
+
