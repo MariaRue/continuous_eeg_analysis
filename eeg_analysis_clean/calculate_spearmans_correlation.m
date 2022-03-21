33c52a2a7c045c1955ab84afad313efb2609e8bc
lhuntneuro <laurence.hunt@psych.ox.ac.uk>
2022-03-21 17:00:33 +0000

    added a version of figure 4 for Lilian poster

--- eeg_analysis_clean/calculate_spearmans_correlation.m
+++ eeg_analysis_clean/calculate_spearmans_correlation.m
@@ -4,14 +4,14 @@ function [corr_across_subjects] = calculate_spearmans_correlation(data,allTau_re
 [~,ai] = sort(allTau_reduced_dm);sl_sorted = [1:nS]'; sl_sorted(ai) = sl_sorted; %to use ranks
 sl_sorted_dm = sl_sorted - mean(sl_sorted);
 
-    data_ranked = data(:,:);
-    [~,ai] = sort(data_ranked,'ascend');
-    tmp = 1:nS;
-    for i = 1:size(data_ranked,2); 
-        data_ranked(ai(:,i),i) = tmp; 
-    end
-    rrr = corr(data_ranked,sl_sorted_dm);
-    
-    corr_across_subjects = reshape(rrr,[size(data,2),size(data,3), size(data,4)]);
+data_ranked = data(:,:);
+[~,ai] = sort(data_ranked,'ascend');
+tmp = 1:nS;
+for i = 1:size(data_ranked,2);
+    data_ranked(ai(:,i),i) = tmp;
+end
+rrr = corr(data_ranked,sl_sorted_dm);
 
-end 
+corr_across_subjects = reshape(rrr,[size(data,2),size(data,3), size(data,4)]);
+
+end
